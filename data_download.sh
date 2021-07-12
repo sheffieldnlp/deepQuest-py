2daa0517030c6b8421e57d6753d6086650a803f8
amitgajbhiye <amitgajbhiye03@gmail.com>
2021-07-12 19:01:52 +0100

    update scripts

--- data_download.sh
+++ data_download.sh
@@ -1,12 +1,19 @@
 #! /bin/sh
 
 #urls for data download
-DATA_URLS="https://www.quest.dcs.shef.ac.uk/dq_student_birnn/et_en_mlqe.tar.gz https://www.quest.dcs.shef.ac.uk/dq_student_birnn/ro_en_mlqe.tar.gz https://www.quest.dcs.shef.ac.uk/dq_student_birnn/si_en_mlqe.tar.gz https://www.quest.dcs.shef.ac.uk/dq_student_birnn/ne_en_mlqe.tar.gz https://www.quest.dcs.shef.ac.uk/dq_student_birnn/en_zh_mlqe.tar.gz https://www.quest.dcs.shef.ac.uk/dq_student_birnn/et_en_25k_wiki.tar.gz https://www.quest.dcs.shef.ac.uk/dq_student_birnn/ro_en_100k_wiki.tar.gz https://www.quest.dcs.shef.ac.uk/dq_student_birnn/si_en_100k_wiki.tar.gz https://www.quest.dcs.shef.ac.uk/dq_student_birnn/ne_en_100k_wiki.tar.gz https://www.quest.dcs.shef.ac.uk/dq_student_birnn/en_zh_100k_wiki.tar.gz"
-
+DATA_URLS="https://www.quest.dcs.shef.ac.uk/dq_student_birnn/et_en_mlqe.tar.gz 
+https://www.quest.dcs.shef.ac.uk/dq_student_birnn/ro_en_mlqe.tar.gz 
+https://www.quest.dcs.shef.ac.uk/dq_student_birnn/si_en_mlqe.tar.gz 
+https://www.quest.dcs.shef.ac.uk/dq_student_birnn/ne_en_mlqe.tar.gz 
+https://www.quest.dcs.shef.ac.uk/dq_student_birnn/en_zh_mlqe.tar.gz 
+https://www.quest.dcs.shef.ac.uk/dq_student_birnn/et_en_25k_wiki.tar.gz 
+https://www.quest.dcs.shef.ac.uk/dq_student_birnn/ro_en_100k_wiki.tar.gz 
+https://www.quest.dcs.shef.ac.uk/dq_student_birnn/si_en_100k_wiki.tar.gz 
+https://www.quest.dcs.shef.ac.uk/dq_student_birnn/ne_en_100k_wiki.tar.gz 
+https://www.quest.dcs.shef.ac.uk/dq_student_birnn/en_zh_100k_wiki.tar.gz"
 
 
 ddir=datasets/
-trainedmodelsdir=trained_models/
 
 for u in $DATA_URLS
 do
