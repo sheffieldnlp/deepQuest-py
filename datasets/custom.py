a62abce345a0303f4232a78848fbb90a186f5815
feralvam <feralvam@gmail.com>
2021-07-15 11:39:30 +0100

    reorganise dataset loaders

--- datasets/custom.py
+++ datasets/custom.py
@@ -16,7 +16,7 @@ _DESCRIPTION = """
 
 
 class CustomQEConfig(datasets.BuilderConfig):
-    def __init__(self, src_lg="src", tgt_lg="tgt", **kwargs):
+    def __init__(self, src_lg="en", tgt_lg="de", **kwargs):
         super(CustomQEConfig, self).__init__(**kwargs)
         self.src_lg = src_lg
         self.tgt_lg = tgt_lg
@@ -40,7 +40,7 @@ class CustomQE(datasets.GeneratorBasedBuilder):
                     "translation": datasets.Translation(languages=(self.config.src_lg, self.config.tgt_lg)),
                     # "src_tags": datasets.Sequence(datasets.features.ClassLabel(num_classes=2)),
                     # "mt_tags": datasets.Sequence(datasets.features.ClassLabel(num_classes=2)),
-                    "sent_score": datasets.Value("float32"),
+                    "z_mean": datasets.Value("float32"),
                 }
             ),
             supervised_keys=None,
@@ -49,43 +49,34 @@ class CustomQE(datasets.GeneratorBasedBuilder):
 
     def _split_generators(self, dl_manager):
         """Returns SplitGenerators."""
-        return [
-            datasets.SplitGenerator(
-                name=datasets.Split.TRAIN,
-                gen_kwargs={
-                    "filepath": self.config.data_files["train"],
-                    "split": "train",
-                    "source_lg": self.config.src_lg,
-                    "target_lg": self.config.tgt_lg,
-                },
-            ),
-            datasets.SplitGenerator(
-                name=datasets.Split.VALIDATION,
-                gen_kwargs={
-                    "filepath": self.config.data_files["validation"],
-                    "split": "valid",
-                    "source_lg": self.config.src_lg,
-                    "target_lg": self.config.tgt_lg,
-                },
-            ),
-            datasets.SplitGenerator(
-                name=datasets.Split.TEST,
-                gen_kwargs={
-                    "filepath": self.config.data_files["test"],
-                    "split": "test",
-                    "source_lg": self.config.src_lg,
-                    "target_lg": self.config.tgt_lg,
-                },
-            ),
-        ]
+        generators = []
+        for name, split in [
+            (datasets.Split.TRAIN, "train"),
+            (datasets.Split.VALIDATION, "validation"),
+            (datasets.Split.TEST, "test"),
+        ]:
+            if split in self.config.data_files:
+                generators.append(
+                    datasets.SplitGenerator(
+                        name=name,
+                        gen_kwargs={
+                            "filepath": self.config.data_files[split],
+                            "split": "train",
+                            "source_lg": self.config.src_lg,
+                            "target_lg": self.config.tgt_lg,
+                        },
+                    )
+                )
+
+        return generators
 
     def _generate_examples(self, filepath, split, source_lg, target_lg):
         logging.info(f"Generating for {split}...")
         df_translations = pd.read_csv(filepath)
         for id, row in df_translations.iterrows():
             yield id, {
-                "translation": {source_lg: str(row["src"]), target_lg: str(row["tgt"])},
-                "sent_score": float(row["sent_score"]),
+                "translation": {source_lg: str(row["original"]), target_lg: str(row["translation"])},
+                "z_mean": float(row["z_mean"]),
                 # "src_tags": str(row["src_tags"]).split(),
                 # "mt_tags": row["tgt_tags"].split(),
             }
