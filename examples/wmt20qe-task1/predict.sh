6972de06938b9f04039e527c0a83b748de7687e3
feralvam <feralvam@gmail.com>
2021-07-15 11:41:47 +0100

    rewrite examples for sentence-level transformer

--- examples/wmt20qe-task1/predict.sh
+++ examples/wmt20qe-task1/predict.sh
@@ -2,12 +2,11 @@
 set -e
 
 deepquestpy_dir="/experiments/falva/tools/deepQuest-py"
-output_dir="./model"
+model_dir="./model"
+output_dir="./output"
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python "${deepquestpy_dir}/deepquestpy_cli/run_transformer.py" \
-    --model_name_or_path "xlm-roberta-base" --arch_name "transformer-sent" \
+    --model_name_or_path "${model_dir}" --arch_name "transformer-sent" \
     --dataset_name "wmt20_mlqe_task1" --src_lang "en" --tgt_lang "de" --label_column_name "z_mean" \
-    --do_train --num_train_epochs 3 --save_steps 10000 --save_total_limit 1 \
-    --learning_rate 1e-5 --warmup_ratio 0.1 --max_grad_norm 1.0 --adam_epsilon 1e-8 \
-    --do_eval \
+    --do_predict \
     --output_dir "${output_dir}" --overwrite_output_dir \
